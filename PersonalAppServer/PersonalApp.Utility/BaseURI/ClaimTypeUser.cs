a5de372e56962944cb5e5a3580f0896b5ef25953
Vantruongg <88721630+truong2307@users.noreply.github.com>
2022-06-04 12:13:06 +0700

    Delete PersonalAppServer/PersonalApp.Utility directory

--- PersonalAppServer/PersonalApp.Utility/BaseURI/ClaimTypeUser.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PersonalApp.Utility.BaseURI
-{
-    public static class ClaimTypeUser
-    {
-        public const string TYPE_NAME = "name";
-        public const string TYPE_USERID = "userId";
-        public const string TYPE_ROLE = "role";
-    }
-}
--- PersonalAppServer/PersonalApp.Utility/BaseURI/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PersonalApp.Utility.BaseURI
-{
-    public static class Role
-    {
-        public const string ADMIN_ROLE = "Admin";
-        public const string USER_ROLE = "User";
-    }
-}
