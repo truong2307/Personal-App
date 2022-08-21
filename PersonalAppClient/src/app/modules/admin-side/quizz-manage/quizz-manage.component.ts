e858423c940af5aeede008c386f645b178273b38
Vantruongg <toiiuvn013@gmail.com>
2022-08-21 10:48:24 +0700

    update form create quizz

--- PersonalAppClient/src/app/modules/admin-side/quizz-manage/quizz-manage.component.ts
+++ PersonalAppClient/src/app/modules/admin-side/quizz-manage/quizz-manage.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { select, Store } from '@ngrx/store';
+import { GetQuizzTopicsAction } from 'src/stores/quizz-topic/quizz-topic.action';
+import { quizzTopicSelector } from 'src/stores/quizz-topic/quizz-topic.selector';
 
 @Component({
   selector: 'app-quizz-manage',
@@ -8,38 +11,73 @@ import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class QuizzManageComponent implements OnInit {
 
+  multipleChoices : any = [
+    {
+      key : 1,
+     title : 'Answer A',
+     value : 'answerA'
+    },
+    {
+      key : 2,
+      title : 'Answer B',
+      value : 'answerB'
+    },
+    {
+      key : 1,
+      title : 'Answer C',
+      value : 'answerC'
+    },
+    {
+      key : 4,
+      title : 'Answer D',
+      value : 'answerD'
+
+    }
+  ];
+
   createQuizzForm! : FormGroup;
   multiplechoiceQuestion! : FormGroup;
+  topicList! : any;
 
   constructor(
-    private formBuilder : FormBuilder
+    private formBuilder : FormBuilder,
+    private store : Store,
   ) { }
 
   ngOnInit(): void {
+    this.store.dispatch(new GetQuizzTopicsAction(0,10));
+
+    this.store.pipe(select(quizzTopicSelector)).subscribe(
+        result => {
+          this.topicList = result.items;
+        }
+      );
+
     this.initialForm();
   }
 
   initialForm(){
-    //Multiple choice question form
-    this.multiplechoiceQuestion = this.formBuilder.group({
+    //Quizz form
+    this.createQuizzForm = this.formBuilder.group({
+      title : ['', [Validators.required]],
+      examTime : ['', [Validators.required, Validators.min(1)]],
+      topicId : ['', [Validators.required]],
+      level : ['Easy', [Validators.required]],
+      isPublic : false,
+      multiplechoiceQuestions : this.formBuilder.array([this.multiplechoiceQuestion = this.createMultipleChoiceForm()])
+    })
+  }
+
+  createMultipleChoiceForm() : FormGroup{
+    return this.formBuilder.group({
       questionText : ['', [Validators.required]],
       questionImage : '',
       answerA : ['', [Validators.required]],
       answerB : ['', [Validators.required]],
       answerC : ['', [Validators.required]],
       answerD : ['', [Validators.required]],
-      correctAnswer : ['', [Validators.required]],
+      correctAnswer : [1, [Validators.required]],
       mark : ['', [Validators.required]],
-    })
-
-    //Quizz form
-    this.createQuizzForm = this.formBuilder.group({
-      title : ['', [Validators.required]],
-      examTime : ['', [Validators.required, Validators.min(1)]],
-      topicId : ['saab', [Validators.required]],
-      level : ['Hard', [Validators.required]],
-      isPublic : false,
-      multiplechoiceQuestions : this.formBuilder.array([this.multiplechoiceQuestion])
     })
   }
 
@@ -55,16 +93,20 @@ get multiplechoiceQuestions(){
   return this.createQuizzForm.get('multiplechoiceQuestions') as FormArray;
 }
 
-get questionText(){
-  return this.multiplechoiceQuestion.get('questionText') as FormArray;
+getFormControlMultipleChoice(curIndex : number, nameControl : string){
+  return this.multiplechoiceQuestions.controls[curIndex].get(nameControl);
 }
 
 addFormQuestion(){
-  this.multiplechoiceQuestions.push(this.multiplechoiceQuestion);
+  this.multiplechoiceQuestions.push(this.createMultipleChoiceForm());
+}
+
+removeMultipleQuestion(index : number){
+  this.multiplechoiceQuestions.removeAt(index);
 }
 
 submit(){
-
+  console.log(this.createQuizzForm);
 }
 
 }
